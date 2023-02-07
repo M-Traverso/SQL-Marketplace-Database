29e8d0537c20a170bdb27b94904974a607f52e96
mtraver <m.traverso96@gmail.com>
2023-02-06 23:19:09 -0300

    script de insercion de datos

--- SQL-Marketplace-Database.sql
+++ SQL-Marketplace-Database.sql
@@ -14,7 +14,7 @@ email varchar(45),
 phone varchar(15)
 );
 
-/* NUYERS*/
+/* BUYERS*/
 
 create table buyers(
 buyer_id int primary key not null auto_increment,
@@ -40,9 +40,9 @@ PIC_phone varchar(15)
 create table products(
 product_id int primary key not null auto_increment,
 SKU int,
-category varchar(25),
+category varchar(80),
 description_ varchar(60),
-price decimal(2) not null,
+price decimal(10,2) not null,
 quantity int,
 seller_id int,
 foreign key (seller_id) references sellers(seller_id) on delete cascade on update cascade
@@ -55,8 +55,8 @@ purchGross_id int primary key not null auto_increment,
 product_id int,
 buyer_id int,
 date_ date,
-status_ varchar(60),
-payment_method varchar (10),
+status_ varchar(100),
+payment_method varchar (50),
 foreign key (product_id) references products(product_id) on update cascade,
 foreign key (buyer_id) references buyers(buyer_id) on update cascade
 );
@@ -66,7 +66,7 @@ foreign key (buyer_id) references buyers(buyer_id) on update cascade
 create table payments(
 payment_id int primary key not null auto_increment,
 purchGross_id int,
-status_ varchar(60),
+status_ varchar(100),
 foreign key (purchGross_id) references gross_purchases(purchGross_id) on update cascade
 );
 
@@ -77,11 +77,10 @@ purchNet_id int primary key not null auto_increment,
 purchGross_id int,
 payment_id int,
 date_ date,
-status_ varchar(60),
+status_ varchar(100),
 foreign key (purchGross_id) references gross_purchases(purchGross_id) on update cascade,
 foreign key (payment_id) references payments(payment_id) on update cascade
 );
-
 
 /* LOGISTICS */
 
@@ -89,9 +88,9 @@ create table logistics(
 shiping_id int primary key not null auto_increment,
 purchNet_id int,
 id_3PL int,
-shiping_price decimal(2) not null,
+shiping_price decimal(10,2) not null,
 est_date date not null,
-status_ varchar(60),
+status_ varchar(100),
 foreign key (purchNet_id) references net_purchases(purchNet_id) on update cascade,
 foreign key (id_3PL) references _3PL´s(id_3PL) on update cascade
 );
@@ -102,7 +101,7 @@ create table gross_sales(
 gross_id int primary key not null auto_increment,
 purchNet_id int,
 shiping_id int,
-status_ varchar(60),
+status_ varchar(100),
 foreign key (purchNet_id) references net_purchases(purchNet_id) on update cascade,
 foreign key (shiping_id) references logistics(shiping_id) on update cascade
 );
@@ -113,49 +112,111 @@ create table net_sales(
 net_id int primary key not null auto_increment,
 gross_id int,
 shiping_id int,
-status_ varchar(60),
+status_ varchar(100),
 foreign key (gross_id) references gross_sales(gross_id) on update cascade,
 foreign key (shiping_id) references logistics(shiping_id) on update cascade
 );
+
+/* INVERSE LOGISTICS */
 
 create table inverse_log(
 inv_ship_id int primary key not null auto_increment,
 net_id int not null,
 id_3PL int not null,
-shiping_price decimal(2) not null,
+shiping_price decimal(10,2) not null,
 est_date date not null,
-status_ varchar(60) not null,
+status_ varchar(100) not null,
 foreign key (net_id) references net_sales(net_id) on update cascade,
 foreign key (id_3PL) references _3PL´s(id_3PL) on update cascade
 );
+
+/* RETURNS */
 
 create table returns_(
 return_id int primary key not null auto_increment,
 net_id int not null,
 inv_ship_id int not null,
-status_ varchar(60) not null,
+status_ varchar(100) not null,
 foreign key (net_id) references net_sales(net_id) on update cascade,
 foreign key (inv_ship_id) references inverse_log(inv_ship_id) on update cascade
 );
+
+/* REFUNDS */
 
 create table refunds(
 refund_id int primary key not null auto_increment,
 purchNet_id int,
 return_id int,
-status_ varchar(60) not null,
+status_ varchar(100) not null,
 foreign key (purchNet_id) references net_purchases(purchNet_id) on update cascade,
 foreign key (return_id) references returns_(return_id) on update cascade
 );
 
-describe sellers;
-describe buyers;
-describe _3pl´s;
-describe products;
-describe gross_purchases;
-describe payments;
-describe net_purchases;
-describe logistics;
-describe gross_sales;
-describe net_sales;
+/* SELLERS */
+insert into sellers (name_,surname,DNI,email,phone) values
+("Manuel","Traverso",39760688,"m.traverso96@gmail.com","1130310056");
+/* BUYERS*/
+insert into buyers (name_,surname,DNI,email,phone,adress) values
+("Julian","Cabrera",40760578,"j.cabrera@gmail.com","1140421022","Echeverria 850");
+/* 3PL´s */
+insert into _3PL´s (company_name,PIC_email,PIC_phone) values
+("Correo Argentino","mariano.paz@correoargentino.com","1145260059");
+/* PRODUCTS */
+insert into products (SKU,category,description_,price,quantity,seller_id) values
+(0001,"Electronica,audio y video","Auriculares in-ear inalambricos",10149,100,1),
+(0002,"Autos, motos y otros","Ford Focus lii 2.0 Se Plus At6",3650000,0,1),
+(0003,"Celulares y telefonos","Samsung Galaxy M23 5G 128 GB",88999,40,1),
+(0004,"Ropa y accesosrios","Camisa manga corta Quiksilver",12453,10,1),
+(0005,"Hogar, muebles y jardin","Mesa comedor 140 + 6 Sillas",36999,100,1),
+(0006,"Electrodomesticos y aires ac.","Aire acondicionado slam split 4472 frigorias 220v",212999,20,1);
+/* PURCHASES */
+insert into gross_purchases (product_id,buyer_id,date_,status_,payment_method) values
+(1,1,"2023-02-05","pago aprobado","tarjeta de credito"),
+(2,1,"2023-02-05","pago rechazado, elige otro medio de pago","tarjeta de credito"),
+(3,1,"2023-02-05","pago pendiente",null),
+(4,1,"2023-02-05","pago aprobado","tarjeta de credito"),
+(5,1,"2023-02-05","pago aprobado","tarjeta de credito"),
+(6,1,"2023-02-05","pago aprobado","tarjeta de credito");
+/* PAYMENTS */
+insert into payments (purchGross_id,status_) values
+(1,"pago con tarjeta de credito visa: APROBADO"),
+(2,"pago con tarjeta de credito visa: RECHAZADO"),
+(4,"pago con tarjeta de credito visa: APROBADO"),
+(5,"pago con tarjeta de credito visa: APROBADO"),
+(6,"pago con tarjeta de credito visa: APROBADO");
+/* NET PURCHASES */
+insert into net_purchases (purchGross_id,payment_id,date_,status_) values
+(1,1,"2023-02-05","el vendedor esta preparando tu pedido"),
+(4,3,"2023-02-05","el vendedor cancelo la compra, te devolveremos tu dinero"),
+(5,4,"2023-02-05","el vendedor ya despacho tu pedido"),
+(6,5,"2023-02-05","el vendedor ya despacho tu pedido");
+/* LOGISTICS */
+insert into logistics (purchNet_id,id_3PL,shiping_price,est_date,status_) values
+(3,1,300,"2023-02-10","el paquete fue entregado"),
+(4,1,300,"2023-02-10","el paquete esta en camino");
+/* GROSS SALES */
+insert into gross_sales (purchNet_id,shiping_id,status_) values
+(3,1,"el paquete fue entregado"),
+(4,2,"el paquete esta en camino");
+/* NET SALES */
+insert into net_sales (gross_id,shiping_id,status_) values
+(1,1,"el paquete ya fue entregado");
+/* INVERSE LOGISTICS */
+insert into inverse_log (net_id,id_3PL,shiping_price,est_date,status_) values
+(1,1,300.50,"2023-02-15","recogido");
+/* RETURNS */
+insert into returns_ (net_id,inv_ship_id,status_) values
+(1,1,"el pedido ya ha sido recogido");
+/* REFUNDS */
+insert into refunds (purchNet_id,return_id,status_) values
+(2,null,"tu dinero ha sido depositado en tu billetera virtual"),
+(3,1,"estamos esperando que el vendedor reciba el producto para devolver tu dinero");
+
+
+
+
+
+
+
 
 
