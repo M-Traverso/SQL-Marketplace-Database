4b4eec9dc7ae3b59c6adead90232e953fe4e5ebd
mtraver <m.traverso96@gmail.com>
2023-03-01 12:11:48 -0300

    script de stored procedures + triggers

--- SQL-Marketplace-Database.sql
+++ SQL-Marketplace-Database.sql
@@ -1,9 +1,9 @@
-/* Marketplace */
+/* -----MARKETPLACE----- */
 
 create schema if not exists marketplace;
 use marketplace;
 
-/* Script de creacion de tablas */
+/* -----SCRIPT DE CREACION DE TABLAS----- */
 
 /* SELLERS */
 
@@ -154,7 +154,7 @@ foreign key (purchNet_id) references net_purchases(purchNet_id) on update cascad
 foreign key (return_id) references returns_(return_id) on update cascade
 );
 
-/* Script de insercion de datos */
+/* -----SCRIPT DE INSERCION DE DATOS----- */
 
 /* SELLERS */
 insert into sellers (name_,surname,DNI,email,phone) values
@@ -220,7 +220,7 @@ insert into refunds (purchNet_id,return_id,status_) values
 (2,null,"tu dinero ha sido depositado en tu billetera virtual"),
 (3,1,"estamos esperando que el vendedor reciba el producto para devolver tu dinero");
 
-/* Script de creacion de vistas */
+/* -----SCRIPT DE CREACION DE VISTAS----- */
 
 /* Seller_center es una vista donde los vendedores van a poder consultar informacion sobre el estado de sus ventas, una especie de tabla de echo */
 create or replace view seller_center as
@@ -330,7 +330,7 @@ on T5.purchNet_id = T10.purchNet_id;
 
 select * from buyer_center;
 
-/* Script de funciones */
+/* -----SCRIPT DE FUCNIONES----- */
 
 DELIMITER //
 create function revenue(n numeric(10,2), p numeric(10,2)) returns numeric(10,2)
@@ -431,10 +431,171 @@ end;
 
 /* Purchase_status lo que busca hacer es dar visibilidad al comprador acerca del status mas actulizado del estado de su compra, es un complemento de la vista buyer_center */
 
-select product_id, description_, purchase_status(status01,status02,status03,status04,status05,status06,status07) Purchase_status from buyer_center;
+create or replace view purchase_board as
+select
+product_id,
+description_,
+purchase_status(status01,status02,status03,status04,status05,status06,status07) as Purchase_status
+from buyer_center;
 
+select * from purchase_board;
 
+/* -----SCRIPT DE STORED PROCEDURES----- */
 
+DELIMITER //
+create procedure pack_off (IN dato int, IN status varchar(100) )
+begin
+  update net_purchases
+  set status_=status
+  where purchNet_id=dato;
+end;
+//
+
+/* El comprador realiza la compra del producto con id=4 */
+
+insert into gross_purchases (product_id,buyer_id,date_,status_,payment_method) values
+(4,1,"2023-02-05","pago aprobado","tarjeta de credito");
+
+select * from gross_purchases;
+
+/* Se procede a revisar el pago de la compra con id=12 */
+
+insert into payments (purchGross_id,status_) values
+(12,"pago con tarjeta de credito visa: APROBADO");
+
+select * from payments;
+
+/* Una vez que el pago con id=10 fue aprobado se genera el registro en net_purchases, la compra se realizo con exito, el reigstro se create
+con el estado "el vendedor esta preparando tu pedido" */
+
+insert into net_purchases (purchGross_id,payment_id,date_,status_) values
+(12,10,"2023-02-05","el vendedor esta preparando tu pedido");
+
+select * from net_purchases;
+select * from logistics;
+
+/* Veamos que en la tabla de net_purchases se creo el registro con el id=13, pero como el paquete esta pendiente de ser despachado, todavia
+no se genero el registro en la tabla de logistics*/
+
+/* Una vez despachado el paquete el vendedor comunicara con un boton a traves del marketplace que el paquete se encuentra en manos del operador logistico,
+esto hara uso del stored procedure pack_off*/
+
+call pack_off(13,"el vendedor ya despacho tu pedido"); /* veamos que pack_off dispara la ejecuccion del trigger logistics_insert_01 */
+
+select * from logistics;
+
+/* Si observamos la tabla de logistics ahora podemos observar que se genero el registro con el id=7 */
+
+/* El sotred procedure shiping lo que hace es asignarle al paquete un operador logistico y un costo de envio, esto queda del lado del operador logistico,
+que luego de recoger el paquete por el correo cargara los datos correspondientes a traves del marketplace */
+
+DELIMITER //
+create procedure shiping (IN dato01 int, IN dato02 int, IN price decimal(6,2))
+begin
+   update logistics
+   set id_3PL=dato02,
+   shiping_price=price
+   where shiping_id=dato01; 
+end;
+//
+
+call shiping(7,1,325.50);
+
+/* -----SCRIPT DE TRIGGERS----- */
+
+/* Los triggers van a generar el registro del envio una vez que el vendedor haya despachado el paquete con el operador logistico */
+
+create table logistics(
+shiping_id int primary key not null auto_increment,
+purchNet_id int,
+id_3PL int,
+shiping_price decimal(10,2) not null,
+est_date date not null,
+status_ varchar(100),
+foreign key (purchNet_id) references net_purchases(purchNet_id) on update cascade,
+foreign key (id_3PL) references _3PL´s(id_3PL) on update cascade
+);
+
+/* El trigger se activa cuando se inserta o se modifica un registro de la tabla de net_purchases, lo que hace es generar el registro en la tabla de logistics, es decir, cada vez que 
+se aprueba un pago y el vendedor despacha el pedido, se genera un registro en la tabla de envios  */
+
+DELIMITER //
+create trigger logistics_insert
+after insert on net_purchases
+for each row
+begin
+  IF (new.status_ = "el vendedor ya despacho tu pedido")
+  THEN
+    insert into logistics(purchNet_id,id_3PL,shiping_price,est_date,status_) values (new.purchNet_id,id_3PL,shiping_price,date_add(now(), interval 4 day),'el paquete esta en camino');
+  END IF;
+end;
+//
+
+DELIMITER //
+create trigger logistics_insert_01
+after update on net_purchases
+for each row
+begin
+  IF (new.status_ = "el vendedor ya despacho tu pedido")
+  THEN
+    insert into logistics(purchNet_id,id_3PL,shiping_price,est_date,status_) values (new.purchNet_id,id_3PL,shiping_price,date_add(now(), interval 4 day),'el paquete esta en camino');
+  END IF;
+end;
+//
+
+/* ---------- */
+
+/* Los triggers van a registrar la operacion de los registros de la tabla productos, creacion, modificacion y eliminacion de productos del marketplace */
+
+create table bitacora (
+id int auto_increment not null primary key,
+action varchar(24),
+date datetime,
+product_id int,
+new_value varchar(60),
+old_value varchar(60),
+user varchar(24)
+);
+
+DELIMITER //
+create trigger bitacora_insert
+after insert on products
+for each row
+begin
+  insert into bitacora(action,date,product_id,new_value,old_value,user) values ('create',now(),new.product_id,new.description_,null,system_user());
+end;
+//
+
+DELIMITER //
+create trigger bitacora_update
+before update on products
+for each row
+begin
+  insert into bitacora(action,date,product_id,new_value,old_value,user) values ('edit',now(),new.product_id,new.description_,old.description_,system_user());
+end;
+//
+
+DELIMITER //
+create trigger bitacora_delete
+before delete on products
+for each row
+begin
+  insert into bitacora(action,date,product_id,new_value,old_value,user) values ('delete',now(),old.product_id,null,old.description_,system_user());
+end;
+//
+
+insert into products (SKU,category,description_,price,quantity,seller_id) values
+(0007,"Electrodomesticos y aires ac.","Aire acondicionado slam split 100000 frigorias 220v",500000,20,1);
+
+update products
+set description_='Aire samsung 50000 frigorias 220v usado'
+where product_id=7;
+
+delete from products where product_id=7;
+
+select * from products;
+
+select * from bitacora;
 
 
 
